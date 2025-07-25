47e8db57b8fafe310555ae8565f20f5b3e6324c6
omarkhalil117 <omarkhalil117@gmail.com>
2025-07-25 16:20:58 +0300

    feat: add config module and env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,23 +6,28 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
-    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-    driver: ApolloDriver,
-  }),
-  TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    database: 'nestDB',
-    username: 'postgres',
-    password: 'add-your-password',
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    synchronize: true,
-    autoLoadEntities: true,
-  }),
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env'
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      driver: ApolloDriver,
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DATABASE_TYPE as 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT!, 10),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: process.env.NODE_ENV !== 'production',
+      autoLoadEntities: true,
+    }),
     CarsModule],
   controllers: [AppController],
   providers: [AppService],
