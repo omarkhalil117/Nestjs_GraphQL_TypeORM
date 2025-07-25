2096d1c1871b06ab59b9ae99a11119e547a760de
omarkhalil117 <omarkhalil117@gmail.com>
2025-07-25 19:30:38 +0300

    feat: add validation using class validator

--- src/cars/dto/creatCarDto.dto.ts
+++ src/cars/dto/creatCarDto.dto.ts
@@ -1,16 +1,22 @@
 import { Field, InputType, Int } from "@nestjs/graphql";
+import { IsAlpha, IsInt, IsNumber, MinLength } from "class-validator";
 
 @InputType()
 export class addCarDto {
+  @IsAlpha()
+  @MinLength(2, { message: 'Model name should be at least 2 charachters long.' })
   @Field()
   model: string
 
+  @IsInt()
   @Field(type => Int)
   year: number
 
+  @IsInt()
   @Field(type => Int)
   cc: number
 
+  @IsAlpha()
   @Field({ nullable: true })
   color: string
 
