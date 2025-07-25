3ee1d33f3845608131fefe3ca88603427ef18c80
omarkhalil117 <omarkhalil117@gmail.com>
2025-07-25 16:12:08 +0300

    feat: connect app with postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,25 @@ import { CarsModule } from './cars/cars.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
     autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     driver: ApolloDriver,
-  }), CarsModule],
+  }),
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    database: 'nestDB',
+    username: 'postgres',
+    password: 'add-your-password',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true,
+    autoLoadEntities: true,
+  }),
+    CarsModule],
   controllers: [AppController],
   providers: [AppService],
 })
